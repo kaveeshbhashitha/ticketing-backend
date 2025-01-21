7ac08b98a676f4aa7ae84d1192f1ebf0019c1e95
wickramDSK <wickdsk.2k@gmail.com>
2025-01-22 01:00:42 +0530

    update CORS configuration to allow requests from localhost:5173

--- src/main/java/com/ticketing/ticketing_backend/Controller/ReservationController.java
+++ src/main/java/com/ticketing/ticketing_backend/Controller/ReservationController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("/reservation")
 public class ReservationController {
     @Autowired
