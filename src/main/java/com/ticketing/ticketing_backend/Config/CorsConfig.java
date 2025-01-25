3f7640824f6fd0df940e7ae89396efbd5e63eae8
PasinduPramudithaJ <pramudithapasindu48@gmail.com>
2025-01-25 10:00:43 +0530

    Resolve the dependency issue

--- src/main/java/com/ticketing/ticketing_backend/Config/CorsConfig.java
+++ src/main/java/com/ticketing/ticketing_backend/Config/CorsConfig.java
@@ -1,10 +1,6 @@
 package com.ticketing.ticketing_backend.Config;
-
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +9,7 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply CORS to all endpoints
